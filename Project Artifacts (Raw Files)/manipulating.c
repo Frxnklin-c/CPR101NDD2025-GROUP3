1e5f652c34b851091329977851af43240c8d6f22
iRcreary <Richard.creary@outlook.com>
2025-04-01 14:43:54 -0400

    Update manipulating.c

--- Project Artifacts (Raw Files)/manipulating.c
+++ Project Artifacts (Raw Files)/manipulating.c
@@ -1,16 +1,33 @@
-#include "manipulating.h"
-
-void manipulating() {
-/* Version 1 */
->> insert here
+// MANTPULATING MODULE HEADER
+#include <stdio.h>
+#include "manipulating.c"
 
 
-/* Version 2 */
->> insert here
 
 
-/* Version 3 */
->> insert here
 
 
+
+// Manipulating Module Header
+
+
+ // Include only necessary headers
+
+
+
+/**
+ * @brief Performs operations related to data manipulation.
+ * 
+ * This function is expected to handle certain manipulation tasks.
+ * The implementation should be defined in the corresponding .c file.
+ */
+ 
+
+ // MANIPULATING_H
+
+int main (){
+
+manipulating ();
+
+    return 0;
 }
