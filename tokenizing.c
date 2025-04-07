6e5d5aa8606470667c0a03f808d4d56a79af5f72
Sina <sinarahimi.dev@gmail.com>
2025-04-07 17:36:42 -0400

    Update tokenizing.c to v3

--- tokenizing.c
+++ tokenizing.c
@@ -2,37 +2,38 @@
 
 // Function that demonstrates how to tokenize a string, seperated by comma into individual words
 void tokenizing(void) {
-	//prompting user to enter a string of character
-   printf("*** Start of Tokenizing Phrases Demo ***\n");
 
-    char phrases[BUFFER_SIZE];
-    //defining an array with size of the entered string
-    char* nextPhrase = NULL;
-    int phrasesCounter;
-
-    do {
-        printf("Type a few phrases separated by comma (q - to quit):\n");
-        fgets(phrases, BUFFER_SIZE, stdin);
-        //reading user input from stdin
-        phrases[strlen(phrases) - 1] = '\0';
-       // setting the last character to be null terminator to remove new line charactteer
-        if ((strcmp(phrases, "q") != 0)) {
-        	//continue only if user Input is not "q"
-            nextPhrase = strtok(phrases, ",");
-            //spliting the string into phrases/tokens whenever there is ","
-            phrasesCounter = 1;
-            // counts the number of phrases/tokens
-
-// while nextPhrase is not Equal to "null", prints the nextPhrase counter and value  
-            while (nextPhrase) {
-    
-                printf("Phrase #%d is '%s'\n", phrasesCounter++, nextPhrase);
-                nextPhrase = strtok(NULL, ",");
-                //sets the value of next phrase to null to move to next token
-            }
+   // V3
+printf("*** Start of Tokenizing Sentences Demo ***\n");
+char sentences[BUFFER_SIZE];
+//declare a character array and set array size to the input length by user
+char* nextSentence = NULL;
+//set nextSentence to null
+int sentencesCounter;
+//defining an integer to count the number of words
+do {
+    printf("Type a few sentences separated by dot(q - to quit):\n");
+    //prompt user to enter an input
+    fgets(sentences, BUFFER_SIZE, stdin);
+    //read the user input up to BUFFER_SIZE from stdin and save it to sentences
+    sentences[strlen(sentences) - 1] = '\0';
+    //set the last character to null terminator to remove newline character
+    if ((strcmp(sentences, "q") != 0)) {
+    	//continue if user input is not equal to "q"
+        nextSentence = strtok(sentences, ".");
+        //seprate the string into different tokens whenevr using "." as the delimiter
+        sentencesCounter = 1;
+        //add to the number of tokens
+        while (nextSentence) {
+            printf("Sentence #%d is '%s'\n", sentencesCounter++, nextSentence);
+            //while the nextSentence is not equal to "null"(there are more tokens), print the token number and token
+            nextSentence = strtok(NULL, ".");
+            //continue deviding the string into tokens
         }
-    } while (strcmp(phrases, "q") != 0); // repeat until user input is not "q"
-
-    printf("*** End of Tokenizing Phrases Demo ***\n");
-    
+    }
+} while (strcmp(sentences, "q") != 0);
+//exits the function if user input is "q"
+printf("*** End of Tokenizing Sentences Demo ***\n\n");
+ 
 }
+
